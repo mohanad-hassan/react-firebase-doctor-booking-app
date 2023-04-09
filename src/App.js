56ae0fa4b4d14b4e3e1aea78790627f4ed1815da
mohanad hassan <mohanadhassan969696@gmail.com>
2023-04-09 12:37:56 +0300

    doctorform with firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './Pages/Login'
 import Register from './Pages/Register'
 import ProtectedRoute from './components/ProtectdRoute'
 import Profile from './Pages/Profile'
+import DoctorForm from "./Pages/DoctorForm/DoctorForm";
 import Spinner from "./components/Spinner";
 import { useSelector } from "react-redux";
 
@@ -20,6 +21,7 @@ const {loading } = useSelector((state) => {return state.loader })
   <Route path ='/register' element={<Register/>}/>
   <Route path ='/' element={<ProtectedRoute> <Home/>  </ProtectedRoute>}/>
   <Route path ='/profile' element={<ProtectedRoute> <Profile/>  </ProtectedRoute>}/>
+  <Route path ='/apply-doctor' element={<ProtectedRoute> <DoctorForm/>  </ProtectedRoute>}/>
  </Routes>
      
     </div>
